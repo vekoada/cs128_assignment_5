42c8fea6fc4b8acb62bb55642726c8f92709b1bd
vekoada <adam@vekony.com>
2023-12-12 13:40:06 -0800

    Write boilerplate

---
+++ src/models.py
@@ -0,0 +1,8 @@
+"""Implementation of various Markov models and Language models for query likelihood document rankings.
+"""
+
+__author__ = "Adam Vekony"
+__copyright__ = "Copyright 2023, Westmont College"
+__credits__ = "Adam Vekony"
+__license__ = "MIT"
+__email__ = "avekony@westmont.edu"
---
+++ src/scratch.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+arxiv_data = pd.read_csv(
+    "https://github.com/soumik12345/multi-label-text-classification/releases/download/v0.2/arxiv_data.csv"
+)
+
+all_summaries = arxiv_data['summaries']
+
